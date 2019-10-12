42f1fbe4796fb9f23d5fa5c7d58eb9f6065aa3f4
aleksandr-jasinski <aleksandr.jasinski@gmail.com>
2019-10-12 18:06:15 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,40 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function decimacia(k) {
+  k = k.replace(/\./g,10).replace(/-/g,11);
+  k = k.padStart(10,0);
+  return k;
+}
+
+let morseTable = new Map(Object.entries(MORSE_TABLE));
+let morseTableM = new Map;
+for (let [key, value] of morseTable.entries()) {
+  morseTableM.set(decimacia(key), value);
+}
+morseTableM.set('**********',' ');
+
 function decode(expr) {
     // write your solution here
+  let len = expr.length / 10;
+  var encodedExprA = [];
+
+  for (let i = 0, begin = 0, end = 10; i < len; i++ , begin += 10, end += 10) {
+    encodedExprA.push(expr.slice(begin, end));
+  }
+
+  let decodedExprA = [];
+
+  encodedExprA.forEach(function (element) {
+    for (let [key, value] of morseTableM.entries()) {
+      if (element === key) {
+        decodedExprA.push(value);
+      }
+    }
+  });
+
+  let decodedExpr = decodedExprA.join('');
+return decodedExpr;
 }
 
 module.exports = {
